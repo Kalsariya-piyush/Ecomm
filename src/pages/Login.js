4e59d1c873b84e390a00d5eefaefedf702455b2b
Piyush <piyushklaptop99@gmail.com>
2024-03-07 01:31:58 +0530

    changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,58 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import BreadCrumb from "../components/BreadCrumb";
-import Meta from "../components/Meta";
-import Container from "../components/Container";
-import CustomInput from "../components/CustomInput";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import BreadCrumb from '../components/BreadCrumb';
+import Meta from '../components/Meta';
+import Container from '../components/Container';
+import CustomInput from '../components/CustomInput';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { LoginHandler } from '../functions/auththenticaion';
+import { HandleSetCookie } from '../constants';
+import { toast } from 'react-toastify';
+import { useAuth } from '../context/auth';
 
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  // const { getUser } = useAuth();
+
+  const { handleBlur, handleChange, handleSubmit, errors, touched, values } =
+    useFormik({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+
+      validationSchema: yup.object({
+        email: yup
+          .string()
+          .email('Please Enter valid email address')
+          .required('Please enter a email address'),
+        password: yup.string().required('Please enter a password'),
+      }),
+
+      onSubmit: (value) => {
+        setIsLoading(true);
+        LoginHandler(value)
+          .then((res) => {
+            if (res?.data && res?.data?._id && typeof window !== undefined) {
+              HandleSetCookie('accessToken', res.data.token);
+              // getUser();
+              // router.push('/');
+              setIsLoading(false);
+            }
+          })
+          .catch((err) => {
+            if (err?.response?.data?.message === 'Invalid Credentials') {
+              toast.error(err?.response?.data?.message);
+            }
+            setIsLoading(false);
+          });
+      },
+    });
+
   return (
     <>
-      <Meta title={"Login"} />
+      <Meta title={'Login'} />
       <BreadCrumb title="Login" />
 
       <Container class1="login-wrapper py-5 home-wrapper-2">
@@ -16,13 +60,28 @@ const Login = () => {
           <div className="col-12">
             <div className="auth-card">
               <h3 className="text-center mb-3">Login</h3>
-              <form action="" className="d-flex flex-column gap-15">
-                <CustomInput type="email" name="email" placeholder="Email" />
-                
+              <form
+                onSubmit={handleSubmit}
+                className="d-flex flex-column gap-15"
+              >
                 <CustomInput
-                  type="password"
+                  type="text"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  value={values?.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+
+                <CustomInput
+                  type="text"
+                  id="password"
                   name="password"
                   placeholder="Password"
+                  value={values?.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                 />
                 <div>
                   <Link to="/forgot-password">Forgot Password?</Link>
@@ -31,6 +90,7 @@ const Login = () => {
                     <button className="button border-0" type="submit">
                       Login
                     </button>
+
                     <Link to="/signup" className="button signup">
                       SignUp
                     </Link>

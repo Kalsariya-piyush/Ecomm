7a8fa33f34d1ab52812531cb72dbe7eb25a01fd8
kalsariyadarshan <darshankalsariya29@gmail.com>
2024-03-10 13:18:52 +0530

    show cart data

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,19 +5,21 @@ import BreadCrumb from '../components/BreadCrumb';
 import Container from '../components/Container';
 import Meta from '../components/Meta';
 import { useAuth } from '../context/auth';
-import { GetCart } from '../functions/products';
-import watch from '../images/watch.jpg';
+import { GetCart, RemoveCartProduct } from '../functions/products';
 
 const Cart = () => {
   const { currentUser, isLoadingUser } = useAuth();
 
   const [isLoading, setIsLoading] = useState(true);
+  const [cartItem, setCartItem] = useState([]);
+
+  const [totalAmount, setTotalAmount] = useState(0);
 
   const getUserCart = () => {
     setIsLoading(true);
     GetCart()
       .then((res) => {
-        console.log('Res > ', res);
+        setCartItem(res?.data);
       })
       .catch((err) => {
         console.log('rtt .> ', err);
@@ -30,6 +32,13 @@ const Cart = () => {
   useEffect(() => {
     if (currentUser && currentUser?._id && !isLoadingUser) getUserCart();
   }, [currentUser, isLoadingUser]);
+
+  useEffect(() => {
+    const total = cartItem?.reduce((acc, item) => {
+      return acc + item?.productId?.price * item?.quantity;
+    }, 0);
+    setTotalAmount(total);
+  }, [cartItem]);
 
   return (
     <>
@@ -44,39 +53,63 @@ const Cart = () => {
               <h4 className="cart-col-3">Quantity</h4>
               <h4 className="cart-col-4">Total</h4>
             </div>
-            <div className="cart-data py-3 mb-2 d-flex justify-content-between align-items-center">
-              <div className="cart-col-1 gap-15 d-flex align-items-center">
-                <div className="w-25">
-                  <img src={watch} className="img-fluid" alt="product image" />
-                </div>
-                <div className="w-75">
-                  <p>GDffdhg</p>
-                  <p>Size: hgf</p>
-                  <p>Color: gfd</p>
-                </div>
+            {isLoading && (
+              <div className="cart-data py-3 mb-2 d-flex justify-content-between align-items-center">
+                <span class="loader mx-auto"></span>
               </div>
-              <div className="cart-col-2">
-                <h5 className="price">$ 100</h5>
-              </div>
-              <div className="cart-col-3 d-flex align-items-center gap-15">
-                <div>
-                  <input
-                    className="form-control"
-                    type="number"
-                    name=""
-                    min={1}
-                    max={10}
-                    id=""
-                  />
+            )}
+            {cartItem &&
+              !isLoading &&
+              cartItem?.map((item, index) => (
+                <div
+                  className="cart-data py-3 mb-2 d-flex justify-content-between align-items-center"
+                  key={index}
+                >
+                  <div className="cart-col-1 gap-15 d-flex align-items-center">
+                    <div className="w-25">
+                      <img
+                        src={item?.productId?.images[0]?.url}
+                        className="img-fluid"
+                        alt="product image"
+                      />
+                    </div>
+                    <div className="w-75">
+                      <p>{item?.productId?.title}</p>
+                      <p className="d-flex gap-3">
+                        Color:
+                        <ul className="colors ps-0">
+                          <li style={{ background: item?.color?.title }} />
+                        </ul>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="cart-col-2">
+                    <h5 className="price">$ {item?.productId?.price}</h5>
+                  </div>
+                  <div className="cart-col-3 d-flex align-items-center gap-15">
+                    <div className=" w-25">
+                      {/* <input
+                        className="form-control"
+                        type="number"
+                        name=""
+                        min={1}
+                        max={10}
+                        id=""
+                        value={item?.quantity}
+                      /> */}
+                      {item?.quantity}
+                    </div>
+                    <div onClick={() => RemoveCartProduct(item?._id)}>
+                      <AiFillDelete className="text-danger " />
+                    </div>
+                  </div>
+                  <div className="cart-col-4">
+                    <h5 className="price">
+                      $ {item?.productId?.price * item?.quantity}
+                    </h5>
+                  </div>
                 </div>
-                <div>
-                  <AiFillDelete className="text-danger " />
-                </div>
-              </div>
-              <div className="cart-col-4">
-                <h5 className="price">$ 100</h5>
-              </div>
-            </div>
+              ))}
           </div>
           <div className="col-12 py-2 mt-4">
             <div className="d-flex justify-content-between align-items-baseline">
@@ -84,7 +117,7 @@ const Cart = () => {
                 Continue To Shopping
               </Link>
               <div className="d-flex flex-column align-items-end">
-                <h4>SubTotal: $ 1000</h4>
+                <h4>SubTotal: $ {totalAmount}</h4>
                 <p>Taxes and shipping calculated at checkout</p>
                 <Link to="/checkout" className="button">
                   Checkout
